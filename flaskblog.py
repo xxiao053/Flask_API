5ee951ceec02cb6f551504774a6db30b8c45138f
xxiao053 <xxiao053@marshall.usc.edu>
2025-11-17 06:31:17 -0800

    Design forms and validations of register and login and create html for register and login pages

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,11 @@
 # import a class Flask
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect 
+from forms import RegistrationForm, LoginForm 
+
 # create an instance 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'c765149c9618cedc66ff06f71b2fc50f' # protect our sever from attack 
 
 posts = [
     {
@@ -32,6 +36,26 @@ def home():
 @app.route("/about")
 def about(): 
     return render_template("about.html", title='About')
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    form = RegistrationForm() 
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))  
+    # url_for(xxx) xxx is the func name, not the route name; return the url of corresponding route
+    return render_template("register.html", title='Register', form=form)
+
+@app.route("/login", methods=['Get', 'POST'])
+def login():
+    form = LoginForm() 
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home')) 
+        else:
+            flash('Login unsuccessful. Please check username and password', 'danger')
+    return render_template("login.html", title='Login', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ forms.py
@@ -0,0 +1,19 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class RegistrationForm(FlaskForm): 
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)]) 
+    # design the form and validation  
+    # create a string field for username, cannot be empty, and length between 2 and 20 char
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm): 
+    email = StringField('Email', validators=[DataRequired(), Email()])  # use email instead of username to login 
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember = BooleanField('Remember Me')  # yes, then browser use cookie to remember, so next time no need to enter email+pwd
+    submit = SubmitField('Login')
+
