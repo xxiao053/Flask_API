a8e2009f0f670a345c1abd955323ce4dfb9c79e9
xxiao053 <xxiao053@marshall.usc.edu>
2025-11-17 15:19:24 -0800

    avoid circualr import and execute

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
-from flaskblog import app 
+from flaskblog import app, engine 
+from flaskblog.models import sql_user, sql_post
+from sqlalchemy import text 
+
+def create_table(query):
+    with engine.begin() as conn:
+        conn.execute(text(query))
 
 if __name__ == "__main__":
+    create_table(sql_user)
+    create_table(sql_post) 
     app.run(debug=True)
