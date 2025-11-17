87a09630bbd794fbb70c46e8bdc0dcbb247fe029
xxiao053 <xxiao053@marshall.usc.edu>
2025-11-17 01:06:32 -0800

    Update home and about html, adding some jinja2 syntex like for loop, if condition

--- flaskblog.py
+++ flaskblog.py
@@ -3,6 +3,21 @@ from flask import Flask, render_template
 # create an instance 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
 # route are what we type into our browser to go to different pages
 # decoraters are just a way to add additional functionality to existing functions 
 # "/" is the root page or home page essentially. But we can also create a "home" page as well
@@ -10,11 +25,13 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home(): 
-    return render_template("home.html")  # all html files must be stored under the "templates" folder
+    return render_template("home.html", posts=posts)  
+# all html files must be stored under the "templates" folder, and it render html files
+# pass variable into html  
 
 @app.route("/about")
 def about(): 
-    return "<h1>About Page<h1>"
+    return render_template("about.html", title='About')
 
 if __name__ == "__main__":
     app.run(debug=True)
