a8ac72c3a1c55ff80890c8d2872bd110814de750
xxiao053 <xxiao053@marshall.usc.edu>
2025-11-17 16:42:42 -0800

    Update posts, and readme

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -6,17 +6,23 @@ from flaskblog.models import sql_insert_user
 
 posts = [
     {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
+        'author': 'Xiao Xiao',
+        'title': 'First Blog Post',
+        'content': 'This is the first post content',
+        'date_posted': 'Sep 20, 2025'
     },
     {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    }
+        'author': 'Sean James',
+        'title': 'What Makes You a Better Developer',
+        'content': 'When people first learn programming, they often focus on writing scripts, solving LeetCode problems, or building simple web pages. APIs feel like something “big companies” use. But the truth is: learning how to build even a basic API instantly levels up how you think about software.',
+        'date_posted': 'Oct 21, 2025'
+    },
+    {
+        'author': 'Martine Harden',
+        'title': "It's the foundation of almost every modern system",
+        'content': 'Payments, messaging apps, social networks, e-commerce—everything runs on APIs.\n\nEven inside Meta, Google, or PayPal, teams talk to each other through internal APIs every day.\nOnce you understand APIs, you understand how large systems communicate.',
+        'date_posted': 'Nov 29, 2025'
+    },
 ]
 
 # route are what we type into our browser to go to different pages
